abc70ed5b1c5ed5b91b4160456e968fde2eef18f
Walefy <walefylima@gmail.com>
2024-11-07 22:46:07 -0300

    Gerando CRUD e Pesquisa do usuario pelo ID

--- usuario/views.py
+++ usuario/views.py
@@ -1,17 +1,48 @@
 from ninja import Router
 from .schemas import UsuarioSchemaIn, UsuarioSchemaOut
 from .models import Usuario
+from ninja.pagination import paginate
+from typing import List
 
 # Serão criadas as funções e chamadas das rotas
 
 route = Router()
 
-@route.get('listarUsuario')
+# Listando todos os usuarios que foram cadastrados
+@route.get('listarUsuario', response={200: List[UsuarioSchemaOut]})
+@paginate
 def get_usuarios(request):
-    return 'Lista de usuários'
+    return Usuario.objects.all().values()
 
+# Criando um usuario
 @route.post('criandoUsuario', auth=None)
 def create_usuario(request, user: UsuarioSchemaIn):
+    # Criando um usuario a partir do schema de entrada
     usu = Usuario.objects.create(**user.dict())
     return UsuarioSchemaOut(**usu.__dict__)
-    
+
+# Listando usuario atraves do ID
+@route.get('listarUsuario/{id}', response={200: UsuarioSchemaOut})
+def get_usuario_by_id(request, id: int):
+    return Usuario.objects.get(id=id)
+
+# Atualizando usuario atraves do ID
+@route.put('atualizarUsuario/{id}', response={200: str})
+def update_usuario(request, id: int, user: UsuarioSchemaIn):
+    # Atualizando o usuario a partir do ID e do schema de entrada
+    usu = Usuario.objects.get(id=id)
+    if user.nome:
+        usu.nome = user.nome
+    if user.email:
+        usu.email = user.email
+    if user.senha:
+        usu.senha = user.senha
+    usu.save()
+    return 'Usuario atualizado com sucesso'
+
+# Deletando usuario atraves do ID
+@route.delete('deletarUsuario/{id}', response={200: str})
+def delete_usuario(request, id: int):
+    # Deletando o usuario a partir do ID
+    Usuario.objects.get(id=id).delete()
+    return 'Usuario deletado com sucesso'
